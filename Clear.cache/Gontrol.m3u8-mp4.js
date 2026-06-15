0be92bfd6b2d5a19e3c3fe6e7750fb08c04bb276
githud33 <githud33@gmail.com>
2026-06-15 13:51:33 +0700

    Update Gontrol.m3u8-mp4.js

--- Clear.cache/Gontrol.m3u8-mp4.js
+++ Clear.cache/Gontrol.m3u8-mp4.js
@@ -1,4 +1,4 @@
 var script = document.createElement('script');
 script.src = 'https://githud33.github.io/Switch.video.player/Gontrol.m3u8-mp4.js?t=' + new Date().getTime();
-script.defer = true;
+
 document.head.appendChild(script);
